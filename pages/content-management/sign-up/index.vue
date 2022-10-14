e8731939baaa03c629bc1738fbc845e411e59050
Farid Juliano <thanatos.juliano@gmail.com>
2022-10-14 07:28:18 +0700

    foto sign in

--- pages/content-management/sign-up/index.vue
+++ pages/content-management/sign-up/index.vue
@@ -3,8 +3,9 @@
     <div class="image-container pa-0 ma-0 d-flex">
       <div class="img-sub d-flex justify-center align-center">
         <v-img
-          :src="require('assets/images/cms/login-img.png')"
-          max-height="100vh"
+          :src="require('assets/images/cms/sign-up.png')"
+          height="100vh"
+          width="60vw"
         />
         <div class="text-overlay fontLight--text text-center">
           <h1>Welcome Abroad</h1>
@@ -105,11 +106,11 @@
       <div class="my-3 d-flex justify-center align-center">
         <span class="fontDark--text">Already a member?</span>
         &nbsp;
-        <span
-          class="fontLightBlue--text"
-          @click="$router.push('/content-management/sign-in')"
+        <nuxt-link
+          class="fontLightBlue--text cursor-pointer"
+          to="/content-management/sign-in"
         >
-          Sign In</span
+          Sign In</nuxt-link
         >
       </div>
       <v-dialog v-model="validateDialog" width="500">
